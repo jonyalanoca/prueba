f663def8d89d04445b9abb4c457129da0b5c0928
jonyalanoca <jonyalanoca2@gmail.com>
2023-07-20 03:34:35 -0300

    a

---
+++ ApiPeliculas/ApiPeliculas/Program.cs
@@ -0,0 +1,115 @@
+using ApiPeliculas.Data;
+using ApiPeliculas.Repositorio;
+using ApiPeliculas.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using ApiPeliculas.PeliculasMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configuramos la conexion a sql server
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+{
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql"));
+});
+
+//Agregamos los repositorios
+builder.Services.AddScoped<ICategoriaRepositorio, CategoriaRepositorio>();
+builder.Services.AddScoped<IPeliculaRepositorio, PeliculaRepositorio>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secreta");
+
+//Agregar el AutoMapper
+builder.Services.AddAutoMapper(typeof(PeliculasMapper));
+
+
+//Aquí se configura la Autenciación
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme= JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata= false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey= true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = 
+        "Autenticación JWT usando el esquema Bearer. \r\n\r\n " +
+        "Ingresa la palabra 'Bearer' seguida de un [espacio] y despues su token en el campo de abajo \r\n\r\n" +
+        "Ejemplo: \"Bearer tkdknkdllskd\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+
+//Soporte para CORS
+//Se pueden habilitar: 1-Un dominio, 2-multiples dominios,
+//3-cualquier dominio (Tener en cuenta seguridad)
+//Usamos de ejemplo el dominio: http://localhost:3223, se debe cambiar por el correcto
+//Se usa (*) para todos los dominios
+builder.Services.AddCors(p => p.AddPolicy("PolicyCors", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//Soporte para CORS
+app.UseCors("PolicyCors");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
